d05013ea1e28968747a0e3443322c751049fe603
naresh <nareshreddylgm@gmail.com>
2021-06-03 10:18:00 +0530

    firstcommit

---
+++ src/main/java/com/order/controller/DemoController.java
@@ -0,0 +1,27 @@
+package com.order.controller;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+	
+	
+	
+	
+	
+	@GetMapping("/check")
+	public ResponseEntity<String> doCheck(){
+		
+		String message="hello";
+		
+		//HttpEntity<String> response=new HttpEntity<>();
+		return null;
+		
+		
+		
+	}
+
+}
